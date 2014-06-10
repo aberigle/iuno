d86af072616773ea3c99a06f9a95a907a650c2bf
Alejandro Berzosa Iglesias <alejandro@apps4glass.com>
2014-06-10 17:35:37 +0200

    bootstrap webapp-aberigle via yo

---
+++ app/static/js/app.debug.js
@@ -0,0 +1,4 @@
+(function() {
+  console.log("we are live");
+
+}).call(this);
---
+++ app/static/js/app.js
@@ -0,0 +1,6 @@
+/*
+  app v0.0.0 - 10/6/2014
+
+  Author: _NAME_ - _EMAIL_
+*/
+(function(){console.log("we are live")}).call(this);
